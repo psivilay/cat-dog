556769dcba5b452020426fb48481cde37bcb6794
Peter Sivilay <sivilaypeter@gmail.com>
2016-11-22 22:06:20 -0600

    Add jquery code for meow and woof response in button click, center intro class with purple color

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
-  $()
+  $("button#meow").click(function() {
+    $("ul#cat-attack").prepend("<li>Meow!</li>")
+    $("ul#dog-attack").prepend("<li>Woof!</li>")
+
+  })
+
+  $("button#woof").click(function() {
+    $("ul#dog-attack").prepend("<li>Woof!</li>")
+    $("ul#cat-attack").prepend("<li>Meow!</li>")
+
+  })
 
 })
