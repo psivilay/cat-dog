f71dc1024344f71c8454af2b7e8d29d3b1eb1a7d
Peter Sivilay <sivilaypeter@gmail.com>
2016-11-22 23:31:07 -0600

    Add image and click to remove

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
   $("button#meow").click(function() {
     $("ul#cat-attack").prepend("<li>Meow!</li>")
-    $("ul#dog-attack").prepend("<li>Woof!</li>")
+    $("ul#dog-attack").prepend("<li>Bark!</li>")
+
+    $(".result-photo").after('<img src="img/cat.jpg" id="Results">')
+    $("#Results").click(function() {
+      $(this).remove()
+    })
 
   })
 
   $("button#woof").click(function() {
     $("ul#dog-attack").prepend("<li>Woof!</li>")
-    $("ul#cat-attack").prepend("<li>Meow!</li>")
+    $("ul#cat-attack").prepend("<li>Hiss!</li>")
 
+    $(".result-photo").after('<img src="img/dog.jpg" id="Results">')
+    $("#Results").click(function() {
+      $(this).remove()
+    })
   })
 
 })
